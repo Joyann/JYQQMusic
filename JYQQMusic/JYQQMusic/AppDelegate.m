989638e04d1e4d7f3d2f4e92df7ce5b0e7b1c9f9
Joyann <joyann.init@gmail.com>
2015-11-24 14:58:02 +0800

    初始化项目结构.

---
+++ JYQQMusic/JYQQMusic/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  JYQQMusic
+//
+//  Created by joyann on 15/11/24.
+//  Copyright © 2015年 Joyann. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+
+@end
